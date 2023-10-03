bccb5657b7bb4ac7cfcece9fc3c2600231dac201
gabrielb217 <gabrielbarroso217@gmail.com>
2023-10-03 20:29:03 -0300

    Criação classe D

---
+++ D.java
@@ -0,0 +1,13 @@
+public class D {
+    public int D1;
+    public char D2;
+
+    public void MD1() {
+        System.out.println("MD1");
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
+    
+}
