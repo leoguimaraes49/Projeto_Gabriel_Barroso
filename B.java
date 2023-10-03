72cbd3a97051cc8b4cd0a6e2d1cf7f776d041b93
gabrielb217 <90225556+gabrielb217@users.noreply.github.com>
2023-10-03 19:55:15 -0300

    Criação classe B

---
+++ B.java
@@ -0,0 +1,30 @@
+/**
+ * @author Gabriel Raimundo Pereira Barroso
+ * Classe B
+ */
+public class B {
+    private int B1;
+    private double B2;
+
+    public int getB1() {
+        return B1;
+    }
+    public void setB1(int b1) {
+        B1 = b1;
+    }
+
+    public double getB2() {
+        return B2;
+    }
+    public void setB2(double b2) {
+        B2 = b2;
+    }
+
+    public void MB1() {
+        System.out.println("MB1");
+    }
+
+    public void MB2() {
+        System.out.println("MB2");
+    }
+}
