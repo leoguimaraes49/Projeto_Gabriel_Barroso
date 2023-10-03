26f8c0b29cae11e8344803051197861ca9094c74
gabrielb217 <90225556+gabrielb217@users.noreply.github.com>
2023-10-03 19:53:57 -0300

    Criação classe A

---
+++ A.java
@@ -0,0 +1,31 @@
+/**
+ * @author Gabriel Raimundo Pereira Barroso
+ * Classe A
+ */
+public class A {
+    private int A1;
+    private double A2;
+
+    public int getA1() {
+        return A1;
+    }
+    public void setA1(int a1) {
+        A1 = a1;
+    }
+
+    public double getA2() {
+        return A2;
+    }
+    public void setA2(double a2) {
+        A2 = a2;
+    }
+
+    public void MA1() {
+        System.out.println("MA1");
+    }
+
+    public void MA2() {
+        System.out.println("MA2");
+    }
+}
+
